8f27a836b344db2700a8961267587182aa4dcdcf
Jakub Szczygiel <kubszczygiel@gmail.com>
2019-02-01 21:19:11 +0100

    logging module added. Paths moved to separate class.

--- src/main/java/sample/SqlHandler.java
+++ src/main/java/sample/SqlHandler.java
@@ -25,9 +25,9 @@ public class SqlHandler implements Writeable, Readable {
             createTableInDatabase(connection, TABLE_NAME);
             tasks = readTasks(connection, TABLE_NAME);
             connection.close();
-            logger.log(Level.INFO,"Reading from database finished");
+            logger.log(Level.INFO, "Reading from database finished");
         } catch (SQLException e) {
-            logger.log(Level.SEVERE,"Reading from database failed. Reason: " + e.getMessage(),e);
+            logger.log(Level.SEVERE, "Reading from database failed. Reason: " + e.getMessage(), e);
 
         }
         return tasks;
@@ -40,20 +40,21 @@ public class SqlHandler implements Writeable, Readable {
     }
 
     private Collection<Task> readTasks(Connection connection, String tableName) throws SQLException {
-        logger.log(Level.INFO,"Reading task from database started");
+        logger.log(Level.INFO, "Reading task from database started");
         Collection<Task> tasks = new ArrayList<>();
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery(String.format("SELECT * FROM %s", tableName));
         while (resultSet.next()) {
+
+            String priority = resultSet.getString("PRIORITY");
+            String description = resultSet.getString("DESCRIPTION");
+            String status = resultSet.getString("STATUS");
+            Date date = Date.valueOf(resultSet.getString("DATE"));
+            int taskId = resultSet.getInt("ID");
+            tasks.add(new Task(date, status, priority, description, taskId));
         }
-        String priority = resultSet.getString("PRIORITY");
-        String description = resultSet.getString("DESCRIPTION");
-        String status = resultSet.getString("STATUS");
-        Date date = Date.valueOf(resultSet.getString("DATE"));
-        int taskId = resultSet.getInt("ID");
-        tasks.add(new Task(date,status, priority, description,taskId));
         statement.close();
-        logger.log(Level.FINE,"Tasks read from Database", tasks);
+        logger.log(Level.FINE, "Tasks read from Database", tasks);
         return tasks;
     }
 
@@ -65,35 +66,34 @@ public class SqlHandler implements Writeable, Readable {
             createTableInDatabase(connection, TABLE_NAME);
             addListToDatabase(connection, tasks, TABLE_NAME);
             connection.close();
-            logger.log(Level.INFO,"Task successfully added to database");
+            logger.log(Level.INFO, "Task successfully added to database");
         } catch (SQLException e) {
-            logger.log(Level.SEVERE,"Writing to database failed. Reason: " + e.getMessage(),e);
+            logger.log(Level.SEVERE, "Writing to database failed. Reason: " + e.getMessage(), e);
         }
     }
 
 
-    public int writeOneElement(Task task)
-    {
-        logger.log(Level.INFO,"Writing single task to database started");
-        Collection<Task> oneTaskCollection=new ArrayList<>();
+    public int writeOneElement(Task task) {
+        logger.log(Level.INFO, "Writing single task to database started");
+        Collection<Task> oneTaskCollection = new ArrayList<>();
         oneTaskCollection.add(task);
         write(oneTaskCollection);
-        int taskId=getLastWrittenTaskId();
-        logger.log(Level.INFO,"Task added to database",task);
+        int taskId = getLastWrittenTaskId();
+        logger.log(Level.INFO, "Task added to database", task);
         return taskId;
     }
-    public void removeTask(Task task)
-    {
-        logger.log(Level.INFO,"Removing task from database started");
+
+    public void removeTask(Task task) {
+        logger.log(Level.INFO, "Removing task from database started");
         try {
             Connection connection = createConnection(DATABASE_NAME);
             Statement statement = connection.createStatement();
-            statement.executeUpdate(String.format("DELETE FROM Tasks WHERE id =%d",task.getTaskId()));
+            statement.executeUpdate(String.format("DELETE FROM Tasks WHERE id =%d", task.getTaskId()));
             statement.close();
             connection.close();
-            logger.log(Level.FINE,"Task removed from database",task);
+            logger.log(Level.FINE, "Task removed from database", task);
         } catch (SQLException e) {
-            logger.log(Level.SEVERE,"Removing task from database failed. Reason: " + e.getMessage(),e);
+            logger.log(Level.SEVERE, "Removing task from database failed. Reason: " + e.getMessage(), e);
         }
 
     }
@@ -125,41 +125,41 @@ public class SqlHandler implements Writeable, Readable {
             String description = task.getDescription();
             String status = task.getStatus();
             statement.executeUpdate(String.format("INSERT INTO %s (DATE, STATUS, PRIORITY, DESCRIPTION) " +
-                    "VALUES ('%s', '%s', '%s', '%s')", tableName, date, status, priority, description));
+                    "VALUES (\"%s\", \"%s\", \"%s\", \"%s\")", tableName, date, status, priority, description));
         }
         statement.close();
     }
 
     public int getLastWrittenTaskId() {
-        logger.log(Level.FINE,"Getting last written task ID");
-        int taskID=0;
+        logger.log(Level.FINE, "Getting last written task ID");
+        int taskID = 0;
         try {
             Connection connection = createConnection(DATABASE_NAME);
             Statement statement = connection.createStatement();
-            ResultSet resultSet =statement.executeQuery(String.format("SELECT MAX (ID) as MaxId FROM %s", TABLE_NAME ));
-            taskID=resultSet.getInt("MaxId");
+            ResultSet resultSet = statement.executeQuery(String.format("SELECT MAX (ID) as MaxId FROM %s", TABLE_NAME));
+            taskID = resultSet.getInt("MaxId");
             System.out.println(taskID);
             statement.close();
             connection.close();
-            logger.log(Level.FINE,"Getting task ID finished successfully");
+            logger.log(Level.FINE, "Getting task ID finished successfully");
         } catch (SQLException e) {
-            logger.log(Level.SEVERE,"Getting task ID failed. Reason: " + e.getMessage(),e);
+            logger.log(Level.SEVERE, "Getting task ID failed. Reason: " + e.getMessage(), e);
         }
         return taskID;
     }
 
     public void changeTask(int taskId, String date, String status, String priority, String description) {
-        logger.log(Level.FINE,"Task changing started");
+        logger.log(Level.FINE, "Task changing started");
         try {
             Connection connection = createConnection(DATABASE_NAME);
             Statement statement = connection.createStatement();
             statement.executeUpdate(String.format("UPDATE %s SET DATE = '%s', STATUS= '%s', PRIORITY='%s', DESCRIPTION='%s' " +
-                    "WHERE ID = %d", TABLE_NAME,date,status,priority,description,taskId));
+                    "WHERE ID = %d", TABLE_NAME, date, status, priority, description, taskId));
             statement.close();
             connection.close();
-            logger.log(Level.FINE,"task changed successfully");
+            logger.log(Level.FINE, "task changed successfully");
         } catch (SQLException e) {
-            logger.log(Level.SEVERE,"Changing task failed. Reason: " + e.getMessage(),e);
+            logger.log(Level.SEVERE, "Changing task failed. Reason: " + e.getMessage(), e);
         }
     }
 }
